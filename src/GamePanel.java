941dd71315a544ca61e306380e1ddd9bf6d2df14
SidneyC624 <sidney.0503c@gmail.com>
2023-03-10 19:14:40 +0800

    generated apple and snake

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -49,14 +49,50 @@ public class GamePanel extends JPanel implements ActionListener{
 			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
 		}
 		
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+		
+		for(int i=0; i<bodyParts; i++) {
+			if(i==0) {
+				g.setColor(Color.green);
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+			else {
+				g.setColor(new Color(45, 180, 0));
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+		}
+		
 	}
 	
 	public void newApple() {
-		
+		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 	}
 	
 	public void move() {
 		
+		// shifts the body of the snake according to the head
+		for(int i=bodyParts; i>0; i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		
+		//moves the head of snake
+		switch(direction) {
+		case'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
 	}
 	
 	public void checkApple() {
@@ -69,8 +105,12 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		
+		if(running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 	}
 	
 	public class MyKeyAdapter extends KeyAdapter{
